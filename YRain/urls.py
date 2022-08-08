beb10d6ddf2229d27283017290434e4d5fcb4515
mj9436 <mj9436@naver.com>
2022-08-08 23:16:30 +0900

    tmp

--- YRain/urls.py
+++ YRain/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 import server.views
 
@@ -27,5 +29,7 @@ urlpatterns = [
     path('borrow/dasan/', server.views.dasan, name="dasan"),
     path('borrow/yangjae/', server.views.yangjae, name="yangjae"),
     path('record/', server.views.record, name="record"),
-    path('user_info', server.views.user_info, name="user_info"),
+    path('user_info/', server.views.user_info, name="user_info"),
 ]
+
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
